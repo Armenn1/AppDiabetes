8956570e3ad019adeda771627ad7074c2c7eb3f2
Armenn1 <armengolllll@gmail.com>
2026-01-22 17:53:37 +0100

    settingsbutton
    cambiado de posición el boton, añadido icono, y deja modificar parametros al usuario

--- TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/MainActivity.kt
+++ TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tfg_diabetesapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSettings = findViewById<Button>(R.id.btnSettings)
 
+        val btnSettings = findViewById< ImageButton>(R.id.btnSettings)
         btnSettings.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
