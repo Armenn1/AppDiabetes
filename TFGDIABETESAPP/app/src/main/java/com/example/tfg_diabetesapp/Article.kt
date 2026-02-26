12bd24b91469d7492a9ffe3a72391fa9b60d2709
Armenn1 <armengolllll@gmail.com>
2026-02-26 18:41:37 +0100

    comment

--- TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/Article.kt
+++ TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/Article.kt
@@ -3,6 +3,6 @@ package com.example.tfg_diabetesapp
 data class Article(
     val title: String,
     val description: String,
-    val type: String, // (video, noticia, etc..)
+    val type: String, //    .. video, noticia, etc
     val url: String,
 )
