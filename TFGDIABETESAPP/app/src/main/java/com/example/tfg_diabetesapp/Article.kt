11c42ff6e2fb3653f74a5ae242785fb5452ad0c6
Armenn1 <armengolllll@gmail.com>
2026-02-26 16:52:42 +0100

    data-class-article

---
+++ TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/Article.kt
@@ -0,0 +1,8 @@
+package com.example.tfg_diabetesapp
+
+data class Article(
+    val title: String,
+    val description: String,
+    val type: String, // (video, noticia, etc..)
+    val url: String,
+)
