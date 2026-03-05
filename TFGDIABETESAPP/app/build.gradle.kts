cd11c56144b652d4f1a21a4a698fef3a10807aa0
Armenn1 <armengolllll@gmail.com>
2026-03-05 17:54:01 +0100

    menu_color+implementationai

--- TFGDIABETESAPP/app/build.gradle.kts
+++ TFGDIABETESAPP/app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
