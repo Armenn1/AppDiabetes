df1c5116fbe99870a978b56543b942a80ae17281
Armenn1 <armengolllll@gmail.com>
2026-04-18 14:03:52 +0200

    Delete Article.kt

--- TFGDIABETESAPP/app/src/main/java/com/example/tfg_diabetesapp/Article.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tfg_diabetesapp
-
-data class Article(
-    val title: String,
-    val description: String,
-    val type: String, //    .. video, noticia, etc
-    val url: String,
-)
